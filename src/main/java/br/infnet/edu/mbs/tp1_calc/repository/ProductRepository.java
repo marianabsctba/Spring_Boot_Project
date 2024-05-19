638cef0ced451254ded1975bb334a6046ff839c0
Mah Bs <62853909+marianabsctba@users.noreply.github.com>
2024-05-19 14:29:15 -0300

    Update ProductRepository.java

--- src/main/java/br/infnet/edu/mbs/tp1_calc/repository/ProductRepository.java
+++ src/main/java/br/infnet/edu/mbs/tp1_calc/repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package br.infnet.edu.mbs.tp1_calc.repository;
 
-public class ProductRepository {
+import br.infnet.edu.mbs.tp1_calc.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
+
