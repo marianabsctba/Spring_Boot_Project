0f52757d255eeb7e556518e09f7b28567c1a77a4
Mah Bs <62853909+marianabsctba@users.noreply.github.com>
2024-05-19 14:26:52 -0300

    Update ProductService.java

--- src/main/java/br/infnet/edu/mbs/tp1_calc/service/ProductService.java
+++ src/main/java/br/infnet/edu/mbs/tp1_calc/service/ProductService.java
@@ -1,30 +1,43 @@
 package br.infnet.edu.mbs.tp1_calc.service;
 
+import br.infnet.edu.mbs.tp1_calc.model.Product;
+import br.infnet.edu.mbs.tp1_calc.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
-public class CalculatorService {
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
 
-    public double add(double a, double b) {
-        return a + b;
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
     }
 
-    public double subtract(double a, double b) {
-        return a - b;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
-    public double multiply(double a, double b) {
-        return a * b;
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
     }
 
-    public double divide(double a, double b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero");
+    public Product updateProduct(Long id, Product productDetails) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (product != null) {
+            product.setName(productDetails.getName());
+            product.setDescription(productDetails.getDescription());
+            product.setPrice(productDetails.getPrice());
+            return productRepository.save(product);
         }
-        return a / b;
+        return null;
     }
 
-    public double power(double base, double exponent) {
-        return Math.pow(base, exponent);
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 }
