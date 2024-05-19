1941f08634bbde0f9f0bca000095d38986dc7af5
Mah Bs <62853909+marianabsctba@users.noreply.github.com>
2024-05-19 14:30:13 -0300

    Update ProductController.java

--- src/main/java/br/infnet/edu/mbs/tp1_calc/controller/ProductController.java
+++ src/main/java/br/infnet/edu/mbs/tp1_calc/controller/ProductController.java
@@ -1,2 +1,50 @@
-package br.infnet.edu.mbs.tp1_calc.controller;public class ProductController {
+package br.infnet.edu.mbs.tp1_calc.controller;
+
+import br.infnet.edu.mbs.tp1_calc.model.Product;
+import br.infnet.edu.mbs.tp1_calc.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        if (updatedProduct != null) {
+            return ResponseEntity.ok(updatedProduct);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
 }
